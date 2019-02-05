6564d3ae35dd71fd30110b50e4caa03a3af25a4a
Ingimar Erlingsson <inkimar@gmail.com>
2019-02-05 13:24:34 +0100

    [TESTING] piping to json_pp

--- testing/post-SSL-1-image.sh
+++ testing/post-SSL-1-image.sh
@@ -1,3 +1,1 @@
-# test to submit three images with metadata to the server
-#curl -L -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @picapica-fly.vegadare-test.b64 http://api.nrm.se/MediaServerResteasy/media
-curl -L -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @picapica-fly.b64 https://api.nrm.se/MediaServerResteasy/media
+curl -L -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @picapica-fly.b64 https://api.nrm.se/MediaServerResteasy/media | json_pp
